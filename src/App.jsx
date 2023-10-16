1597241aed26d2ffb3101404c6a8d76a41541e75
Truong Quang Dat <datmin220503@gmail.com>
2023-10-16 19:28:25 +0700

    add source code

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { Header } from "./HeaderComponent/Header"
+import { Banner } from "./BannerComponent/Banner"
+import { Item } from "./ItemComponent/Item"
+import { Footer } from "./FooterComponent/Footer"
+
+function App() {
+
+  return (
+
+    <div>
+      <Header />
+      <Banner />
+      <Item />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/FooterComponent/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div>
+            {/* <!-- Footer--> */}
+            <footer className="py-5 bg-dark">
+                <div className="container"><p className="m-0 text-center text-white">Copyright © Daat Miin 2023</p></div>
+            </footer>
+
+        </div>
+    )
+}
